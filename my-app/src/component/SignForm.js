6a9ad6425baaa31fb50d9c6eda0c7f27dd81b593
William Ryan <billy.ryan.rom@gmail.com>
2020-01-31 02:12:51 -0500

    Add Form to SignForm.js

--- my-app/src/component/SignForm.js
+++ my-app/src/component/SignForm.js
@@ -11,6 +11,47 @@ const SignForm = ({ values, errors, touched, status }) => {
         status && setSignUp(signUp => [...signUp, status]);
     }, [status]);
 
+    return (
+        <div className="signup-form">
+            <Form>
+                <label htmlFor="name">
+                    Name
+                    <Field 
+                      id="name"
+                      type="text"
+                      name="name"
+                      placeholder="name"
+                    />
+                    {touched.name && errors.name && (
+                        <p className="errors"> {errors.name}</p>
+                    )}
+                </label>
+                <label htmlFor="email">
+                    Email
+                    <Field id="email" type="email" name="email" placeholder="email" />
+                    {touched.email && errors.email && (
+                        <p className="errors">{errors.email}</p>
+                    )}
+                </label>
+                <label htmlFor="password">
+                    <Field id="password" type="password" name="size" placeholder="password" />
+                    {touched.password && errors.password && (
+                        <p className="errors">{errors.password}</p>
+                    )}
+                </label>
+                <label className="checkbox-container">
+                    Terms Of Service
+                    <Field 
+                    type="checkbox"
+                    name="terms"
+                    checked={values.terms}
+                    />
+                    <span className="checkmark" />
+                </label>
+                <button type="submit">Submit!</button>
+            </Form>
+        </div>
+    )
 }
 
 export default SignForm;
